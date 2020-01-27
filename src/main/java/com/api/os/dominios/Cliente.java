20e5d4731fcd26c8384f86ca3a0bad6084acb171
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-01-27 16:31:31 -0300

    Inserção de Clientes - Ajustando a hora certa de OS

--- src/main/java/com/api/os/dominios/Cliente.java
+++ src/main/java/com/api/os/dominios/Cliente.java
@@ -14,9 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "cliente")
-@Getter
-@Setter
+@Getter @Setter
 @EqualsAndHashCode
 @ClienteAnnotation
 public class Cliente implements Serializable {
